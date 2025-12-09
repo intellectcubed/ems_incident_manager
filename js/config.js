81bc0b37b6d5b5712d169abd9b18b4f03eacb82e
George Nowakowski <gnowakowski@gmail.com>
2025-12-09 14:17:13 -0500

    Adding supabase

--- js/config.js
+++ js/config.js
@@ -6,5 +6,5 @@
  * - SUPABASE_ANON_KEY: Your Supabase anonymous/public API key
  */
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL_HERE';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://wcvwtpgbghgypdyfxjha.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indjdnd0cGdiZ2hneXBkeWZ4amhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyNDE0MTAsImV4cCI6MjA4MDgxNzQxMH0.Jz0xC8oROHDMKxLJMHQcwoLajdoKq_BrYdd-8vhqSaU';
